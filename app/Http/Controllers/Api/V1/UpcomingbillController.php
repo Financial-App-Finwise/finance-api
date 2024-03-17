fae396bc29e56bbc4eaf3364072c9bbbe3aa1e6f
monivirakpech-peav <monivirakpichzzzzz@gmail.com>
2024-03-17 19:31:00 +0700

    fix bug in upcomingbill index

--- app/Http/Controllers/Api/V1/UpcomingbillController.php
+++ app/Http/Controllers/Api/V1/UpcomingbillController.php
@@ -21,108 +21,7 @@ class UpcomingbillController extends Controller
     /**
      * Display a listing of the resource.
      */
-    // public function index(Request $request)
-    // {
-    //     $user = auth()->user();
-    //     $upcomingbill = UpcomingBill::where('userID', $user->id);
-    //     $filter = new UpcomingBillFilter();
-    //     $queryItems = $filter->transform($request); //[['column', 'operator', 'value']]
-
-    //     if (count($queryItems) == 0){
-    //         return new UpcomingbillCollection(UpcomingBill::paginate());
-    //     } else{ 
-
-    //         $upcomingbill = UpcomingBill::where($queryItems)->paginate();
-
-    //         return new UpcomingbillCollection($upcomingbill->appends($request->query()));
-    //     }
-    // }
-    //$upcomingbill = UpcomingBill::where($queryItems)->paginate();
-
-
-    // public function index(Request $request)
-    // {
-    //     $user = auth()->user();
-
-    //     $filter = new UpcomingBillFilter();
-    //     $queryItems = $filter->transform($request); //[['column', 'operator', 'value']]
-
-    //     $query = UpcomingBill::where('userID', $user->id);
-
-    //     // Apply filters from the query parameters
-    //     foreach ($queryItems as $item) {
-    //         $query->where($item[0], $item[1], $item[2]);
-    //     }
-    //     // Filter by specific month and year if provided
-    //     $month = $request->input('month');
-    //     $year = $request->input('year');
-    //     if ($month && $year) {
-    //         $query->whereMonth('date', '=', date('m', strtotime($month)))
-    //             ->whereYear('date', '=', $year);
-    //     }
-    //     // Filter by status (paid and unpaid)
-    //     $status = $request->input('status');
-    //     if ($status) {
-    //         $query->where('status', $status);
-    //     }
-    //     // Sort by date
-    //     $dateSort = $request->input('date');
-    //     if ($dateSort === 'desc') {
-    //         $query->orderBy('date', 'desc');
-    //     } else {
-    //         $query->orderBy('date', 'asc');
-    //     }
-
-    //     //Filter group by Year->Month
-    //     $year = $request->get('year');
-    //     $month = $request->get('month');
-    //     if ($year) {
-    //         $startDate = Carbon::createFromFormat('Y', $year)->startOfYear();
-    //         $endDate = Carbon::createFromFormat('Y', $year)->endOfYear();
-    //         $query->whereBetween('date', [$startDate, $endDate]);
-
-    //         $upcomingbillsByMonth = $query->get()->groupBy(function ($upcomingbill) {
-    //             return Carbon::parse($upcomingbill->date)->format('F');
-    //         });
-
-    //         if ($month) {
-    //             $upcomingbillsForMonth = $upcomingbillsByMonth->get($month, collect());
-    //             return [
-    //                 'Total Upcomingbill' => $upcomingbillsForMonth->count(),
-    //                 'upcomingbills' => $upcomingbillsForMonth,
-    //             ];
-    //         }
-
-    //         $months = collect();
-    //         for ($i = 1; $i <= 12; $i++) {
-    //             $monthName = Carbon::createFromFormat('m', $i)->format('F');
-    //             $upcomingbillsForMonth = $upcomingbillsByMonth->get($monthName, collect());
-    //             $months->put($monthName, ['Number of upcomingbills' => $upcomingbillsForMonth->count()]);
-    //         }
-
-    //         return $months;
-    //     }
-
-    //     // Fetch total count of upcoming bills
-    //     $totalCount = $query->count();
-
-    //     // Fetch paginated upcoming bills
-    //     $upcomingbills = $query->orderBy('date', 'asc')->paginate();
-
-    //     return response()->json([
-    //         'totalUpcomingBills' => $totalCount,
-    //         'upcomingBills' => new UpcomingBillCollection($upcomingbills->appends($request->query())),
-    //         'links' => [
-    //             'first' => $upcomingbills->url(1),
-    //             'last' => $upcomingbills->url($upcomingbills->lastPage()),
-    //             'prev' => $upcomingbills->previousPageUrl(),
-    //             'next' => $upcomingbills->nextPageUrl(),
-    //         ]
-    //     ]);
-    // }
-    //return new UpcomingbillCollection($upcomingbills->appends($request->query()));
-
-
+ 
     public function index(Request $request)
     {
         $user = auth()->user();
@@ -136,7 +35,6 @@ class UpcomingbillController extends Controller
         foreach ($queryItems as $item) {
             $query->where($item[0], $item[1], $item[2]);
         }
-
         // Filter by specific month and year if provided
         $month = $request->input('month');
         $year = $request->input('year');
@@ -144,13 +42,11 @@ class UpcomingbillController extends Controller
             $query->whereMonth('date', '=', date('m', strtotime($month)))
                 ->whereYear('date', '=', $year);
         }
-
         // Filter by status (paid and unpaid)
         $status = $request->input('status');
         if ($status) {
             $query->where('status', $status);
         }
-
         // Sort by date
         $dateSort = $request->input('date');
         if ($dateSort === 'desc') {
@@ -183,11 +79,42 @@ class UpcomingbillController extends Controller
                     break;
             }
         }
+
+        //Filter group by Year->Month
+        $year = $request->get('year');
+        $month = $request->get('month');
+        if ($year) {
+            $startDate = Carbon::createFromFormat('Y', $year)->startOfYear();
+            $endDate = Carbon::createFromFormat('Y', $year)->endOfYear();
+            $query->whereBetween('date', [$startDate, $endDate]);
+
+            $upcomingbillsByMonth = $query->get()->groupBy(function ($upcomingbill) {
+                return Carbon::parse($upcomingbill->date)->format('F');
+            });
+
+            if ($month) {
+                $upcomingbillsForMonth = $upcomingbillsByMonth->get($month, collect());
+                return [
+                    'Total Upcomingbill' => $upcomingbillsForMonth->count(),
+                    'upcomingbills' => $upcomingbillsForMonth,
+                ];
+            }
+
+            $months = collect();
+            for ($i = 1; $i <= 12; $i++) {
+                $monthName = Carbon::createFromFormat('m', $i)->format('F');
+                $upcomingbillsForMonth = $upcomingbillsByMonth->get($monthName, collect());
+                $months->put($monthName, ['Number of upcomingbills' => $upcomingbillsForMonth->count()]);
+            }
+
+            return $months;
+        }
+
         // Fetch total count of upcoming bills
         $totalCount = $query->count();
 
         // Fetch paginated upcoming bills
-        $upcomingbills = $query->paginate();
+        $upcomingbills = $query->orderBy('date', 'asc')->paginate();
 
         // Calculate totals for different time frames
         $thisMonthTotal = $this->getTotalForMonth(now()->month, now()->year);
@@ -213,7 +140,7 @@ class UpcomingbillController extends Controller
                 'next' => $upcomingbills->nextPageUrl(),
             ]
         ]);
-    }
+    } 
 
     // Helper functions to calculate total upcoming bills for different time frames
     protected function getTotalForMonth($month, $year)
@@ -257,6 +184,9 @@ class UpcomingbillController extends Controller
 
             // Populate the upcomingbill attributes with validated data
             $upcomingbill->fill($validatedData);
+
+            // Set the status to "unpaid"
+            $upcomingbill->status = 'unpaid';
 
             // Save the upcomingbill to the database
             $upcomingbill->save();
